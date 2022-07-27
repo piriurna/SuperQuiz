d23a4252201d16963a581553d6408b7ff95ba1dc
Franco <Zalamena>
2022-07-27 18:15:36 +0100

    New Colors

--- app/src/main/java/com/piriurna/superquiz/ui/theme/Color.kt
+++ app/src/main/java/com/piriurna/superquiz/ui/theme/Color.kt
@@ -6,3 +6,9 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val purple = Color(red = 149, green = 67, blue = 255)
+val lightPurple = Color(red = 149, green = 67, blue = 255, alpha = 26)
+
+val orange = Color(red = 254, green = 177, blue = 87)
+val lightOrange = Color(red = 254, green = 177, blue = 87, alpha = 26)
