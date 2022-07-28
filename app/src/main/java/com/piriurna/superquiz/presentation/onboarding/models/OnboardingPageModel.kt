426163d48974422a6423e2b0bce079c95f9b6e7b
Franco <Zalamena>
2022-07-28 22:48:16 +0100

    OnboardingPageModel changes

--- app/src/main/java/com/piriurna/superquiz/presentation/onboarding/models/OnboardingPageModel.kt
+++ app/src/main/java/com/piriurna/superquiz/presentation/onboarding/models/OnboardingPageModel.kt
@@ -6,6 +6,21 @@ data class OnboardingPageModel(
     val primaryColor : Color,
     val backgroundColor : Color,
     val pageIndex : Int,
+    var pageCount : Int = 0,
+    var nextPage: Int = FINAL_PAGE_INDICATOR,
     val pageTitle : String,
-    val pageDescription : String
-)
+    val pageDescription : String,
+    val mainImage : Int = 0,
+    var viewPagerModel: OnboardingViewPagerModel? = null
+) {
+
+    fun isLastPage() : Boolean {
+        return nextPage == FINAL_PAGE_INDICATOR
+    }
+
+
+    companion object {
+        //I tried setting it to -1 to be visible but it didn't let me
+        const val FINAL_PAGE_INDICATOR = 0
+    }
+}
