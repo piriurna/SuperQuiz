c647f1915d299dc2174134cf4f7f83a4cb69fbc2
Franco <Zalamena>
2022-07-28 22:49:31 +0100

    OnboardingOptions with click and viewpager integration

--- app/src/main/java/com/piriurna/superquiz/presentation/onboarding/composables/OnboardingOptions.kt
+++ app/src/main/java/com/piriurna/superquiz/presentation/onboarding/composables/OnboardingOptions.kt
@@ -1,5 +1,6 @@
 package com.piriurna.superquiz.presentation.onboarding.composables
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -12,21 +13,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.piriurna.superquiz.R
 import com.piriurna.superquiz.presentation.onboarding.models.OnboardingPageModel
 import com.piriurna.superquiz.ui.theme.lightPurple
-import com.piriurna.superquiz.ui.theme.purple
+import com.piriurna.superquiz.ui.theme.orange
+import kotlinx.coroutines.launch
 
 @Composable
 fun OnboardingOptions(
-    isFinalPage : Boolean,
-    primaryColor: Color,
+    onboardingPageModel: OnboardingPageModel,
     modifier: Modifier = Modifier
 ) {
     Box(modifier = modifier) {
-        if(isFinalPage) {
-            OnboardingStartOption(primaryColor)
+        if(onboardingPageModel.isLastPage()) {
+            OnboardingStartOption(onboardingPageModel)
         } else {
-            OnboardingSkipOption(primaryColor)
+            OnboardingSkipOption(onboardingPageModel)
         }
     }
 }
@@ -34,21 +37,44 @@ fun OnboardingOptions(
 @Preview(showBackground = true)
 @Composable
 private fun OnboardingOptionsPreview() {
-    OnboardingOptions(false, purple)
+    val model = OnboardingPageModel(
+        pageTitle = "Hmmmm, Healthy food",
+        pageDescription = "A variety of healthy foods made by the best chefs, ingredients are easy to find, all delicious flavours can only be found at cookbunda",
+        primaryColor = orange,
+        backgroundColor = lightPurple,
+        pageIndex = 5,
+        mainImage = R.drawable.ic_banana_svgrepo_com
+    )
+    OnboardingOptions(model)
 }
 
 @Composable
-fun OnboardingSkipOption(primaryColor : Color) {
+fun OnboardingSkipOption(pageModel: OnboardingPageModel) {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment= Alignment.CenterVertically,
         modifier = Modifier.fillMaxWidth()
     ) {
-        TextButton(onClick = { /*TODO*/ }, ) {
+        TextButton(onClick = { } ) {
             Text(text = "Skip now", color = Color.Black)
         }
         
-        SQRoundButton(primaryColor = primaryColor)
+        SQRoundButton(
+            primaryColor = pageModel.primaryColor,
+            onClickListener = { goToNextPage(pageModel) } )
+    }
+}
+
+
+@SuppressLint("Range")
+@OptIn(ExperimentalPagerApi::class)
+private fun goToNextPage(pageModel: OnboardingPageModel) {
+    pageModel.viewPagerModel?.let {
+        with(it){
+            scope.launch {
+                state.animateScrollToPage(pageModel.nextPage)
+            }
+        }
     }
 }
 
@@ -59,12 +85,14 @@ private fun OnboardingSkipOptionPreview() {
 }
 
 @Composable
-fun OnboardingStartOption(primaryColor: Color) {
+fun OnboardingStartOption(onboardingPageModel: OnboardingPageModel) {
     Button(
-        onClick = { /*TODO*/ },
+        onClick = { goToNextPage(onboardingPageModel) },
         shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.buttonColors(backgroundColor = primaryColor),
-        modifier = Modifier.fillMaxWidth()
+        colors = ButtonDefaults.buttonColors(backgroundColor = onboardingPageModel.primaryColor),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(45.dp)
     ) {
         Text(text = "Get Started", color = Color.White)
     }
