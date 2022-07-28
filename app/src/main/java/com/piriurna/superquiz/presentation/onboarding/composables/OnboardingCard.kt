ccb286dbb4ec118e666403ba71399039dd4a302b
Franco <Zalamena>
2022-07-28 22:48:47 +0100

    OnboardingCard fixes

--- app/src/main/java/com/piriurna/superquiz/presentation/onboarding/composables/OnboardingCard.kt
+++ app/src/main/java/com/piriurna/superquiz/presentation/onboarding/composables/OnboardingCard.kt
@@ -20,25 +20,31 @@ import com.piriurna.superquiz.ui.theme.purple
 
 @Composable
 fun OnboardingCard(
-    onboardingPageModel: OnboardingPageModel
+    onboardingPageModel: OnboardingPageModel,
+    modifier: Modifier = Modifier
 ) {
-    val pageCount = 6
     Card(
-        shape = RoundedCornerShape(8.dp),
-        modifier = Modifier.padding(16.dp),
+        shape = RoundedCornerShape(topStartPercent = 30),
+        modifier = modifier,
 
     ) {
         Column(
-            modifier = Modifier.fillMaxWidth().padding(12.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            //-----------------
+            // ViewPager Indicator
+            //-----------------
             Row(
                 horizontalArrangement = Arrangement.Center,
                 modifier = Modifier.fillMaxWidth()
             ) {
                 SQViewPagerIndicator(
-                    totalDots = pageCount,
+                    totalDots = onboardingPageModel.pageCount,
                     selectedIndex = onboardingPageModel.pageIndex,
                     selectedColor = onboardingPageModel.primaryColor,
                     unSelectedColor = onboardingPageModel.backgroundColor
@@ -46,24 +52,42 @@ fun OnboardingCard(
                 )
             }
 
+
+            //-----------------
+            // TEXTS CONTAINERS
+            //-----------------
             Text(
                 text = onboardingPageModel.pageTitle,
                 fontSize = 24.sp,
                 modifier = Modifier.padding(top = 16.dp)
             )
-            
-            Text(
-                text = onboardingPageModel.pageDescription,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(horizontal = 36.dp),
-                color = Color.Gray
-            )
 
-            OnboardingOptions(
-                isFinalPage = onboardingPageModel.pageIndex == pageCount - 1,
-                primaryColor = onboardingPageModel.primaryColor,
-                modifier = Modifier.padding(horizontal = 24.dp)
-            )
+            Box(modifier = Modifier
+                .height(75.dp)
+            ) {
+                Text(
+                    text = onboardingPageModel.pageDescription,
+                    textAlign = TextAlign.Center,
+                    color = Color.Gray,
+                    modifier = Modifier
+                        .padding(horizontal = 36.dp)
+                        .align(Alignment.Center)
+                )
+            }
+
+            //-----------------
+            // BUTTONS CONTAINER
+            //-----------------
+            Box(modifier = Modifier
+                .height(120.dp)
+            ) {
+                OnboardingOptions(
+                    onboardingPageModel = onboardingPageModel,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 16.dp)
+                        .align(Alignment.Center)
+                )
+            }
         }
     }
 }
@@ -77,7 +101,8 @@ private fun OnboardingCardPreview() {
         pageDescription = "A variety of healthy foods made by the best chefs, ingredients are easy to find, all delicious flavours can only be found at cookbunda",
         primaryColor = orange,
         backgroundColor = lightPurple,
-        pageIndex = 2
+        pageIndex = 5,
+        pageCount = 6,
     )
     Box(modifier = Modifier.fillMaxSize()) {
         OnboardingCard(model)
