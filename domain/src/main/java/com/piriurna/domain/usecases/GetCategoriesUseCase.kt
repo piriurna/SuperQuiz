26d1d48077fc07cf87d837618cce4283e5a09397
Franco <Zalamena>
2022-08-01 15:28:03 +0100

    GetCategoriesUseCase

---
+++ domain/src/main/java/com/piriurna/domain/usecases/GetCategoriesUseCase.kt
@@ -0,0 +1,27 @@
+package com.piriurna.domain.usecases
+
+import com.piriurna.domain.ApiNetworkResponse
+import com.piriurna.domain.Resource
+import com.piriurna.domain.models.Category
+import com.piriurna.domain.repositories.TriviaRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetCategoriesUseCase(
+    private val triviaRepository: TriviaRepository
+) {
+
+    operator fun invoke() : Flow<Resource<List<Category>>> = flow {
+        emit(Resource.Loading())
+
+        val categoriesResult : ApiNetworkResponse<List<Category>> = triviaRepository.getCategories()
+
+        categoriesResult.data?.let {
+            emit(Resource.Success(it))
+        }?: kotlin.run {
+            emit(Resource.Error(message = categoriesResult.error.message!!))
+        }
+
+
+    }
+}
