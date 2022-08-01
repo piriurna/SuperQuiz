4559c91a6fa7c84402c7109d52f03380077ace23
Franco <Zalamena>
2022-08-01 16:10:19 +0100

    Hilt Activity Entry point

--- app/src/main/java/com/piriurna/superquiz/MainActivity.kt
+++ app/src/main/java/com/piriurna/superquiz/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.piriurna.superquiz.ui.theme.SuperQuizTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
