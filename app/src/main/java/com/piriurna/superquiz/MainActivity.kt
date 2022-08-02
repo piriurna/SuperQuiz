8904f55b17a7567cb8751988bc61fe277dffe1ec
Franco <Zalamena>
2022-08-02 10:27:41 +0100

    PlayGamesScreen in MainActivity

--- app/src/main/java/com/piriurna/superquiz/MainActivity.kt
+++ app/src/main/java/com/piriurna/superquiz/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.piriurna.superquiz.presentation.playgames.PlayGamesScreen
 import com.piriurna.superquiz.ui.theme.SuperQuizTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    PlayGamesScreen()
                 }
             }
         }
