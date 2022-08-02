b7534770eb933a4dbc5654ee27b2bcaff4f3884a
Franco <Zalamena>
2022-08-02 13:49:41 +0100

    Default backgroundColor for ChipModel

--- app/src/main/java/com/piriurna/superquiz/presentation/composables/models/ChipModel.kt
+++ app/src/main/java/com/piriurna/superquiz/presentation/composables/models/ChipModel.kt
@@ -2,11 +2,13 @@ package com.piriurna.superquiz.presentation.composables.models
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.piriurna.superquiz.ui.theme.lightOrange
+import com.piriurna.superquiz.ui.theme.orange
 
 data class ChipModel(
     val icon: ImageVector,
     val text : String,
-    val backgroundColor: Color,
-    val foregroundColor: Color
+    val backgroundColor: Color = lightOrange,
+    val foregroundColor: Color = orange
 ) {
 }
