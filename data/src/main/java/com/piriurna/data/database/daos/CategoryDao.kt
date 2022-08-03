23100f68ac79c1253c76be492a76017ac2a0b430
Franco <Zalamena>
2022-08-03 14:36:30 +0100

    CategoryDao

---
+++ data/src/main/java/com/piriurna/data/database/daos/CategoryDao.kt
@@ -0,0 +1,21 @@
+package com.piriurna.data.database.daos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.piriurna.data.database.entities.CategoryEntity
+
+@Dao
+interface CategoryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCategory(categoryEntity: CategoryEntity)
+
+    @Query("DELETE FROM CATEGORIES")
+    suspend fun deleteCategories()
+
+    @Query("SELECT * FROM CATEGORIES")
+    suspend fun getCategories(): List<CategoryEntity>?
+
+
+}
