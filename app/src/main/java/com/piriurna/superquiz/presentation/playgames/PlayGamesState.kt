3aa9c02efbc1e7b2cb4be4cd8d8a17fc3dc4721a
Franco <Zalamena>
2022-08-03 12:28:42 +0100

    PlayGamesState

---
+++ app/src/main/java/com/piriurna/superquiz/presentation/playgames/PlayGamesState.kt
@@ -0,0 +1,8 @@
+package com.piriurna.superquiz.presentation.playgames
+
+import com.piriurna.domain.models.Category
+
+data class PlayGamesState(
+    val isLoading: Boolean = false,
+    val categories: List<Category> = emptyList()
+)
