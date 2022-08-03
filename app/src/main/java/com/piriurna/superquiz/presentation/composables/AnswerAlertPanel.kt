bfead55de74b1379689f52316c3294c7a91de242
Franco <Zalamena>
2022-08-03 12:08:24 +0100

    AnswerAlertPanel

--- app/src/main/java/com/piriurna/superquiz/presentation/composables/AnswerAlertPanel.kt
+++ app/src/main/java/com/piriurna/superquiz/presentation/composables/AnswerAlertPanel.kt
@@ -2,32 +2,55 @@ package com.piriurna.superquiz.presentation.composables
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.piriurna.superquiz.ui.theme.primaryGreen
 
 @Composable
-fun AnswerAlertPanel() {
+fun AnswerAlertPanel(
+    modifier: Modifier = Modifier,
+    topText : String,
+    topBadge: ImageVector,
+    middleText : String? = null,
+    bottomText: String? = null
+) {
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth()
     ) {
 
-        Text(
-            text = "Correct answer"
+        SQBadgeText(
+            icon = topBadge,
+            text = topText,
+            foregroundColor = primaryGreen
         )
 
-       Text(
-           text = "All good things come to those who wait.",
-           textAlign = TextAlign.Center,
-           softWrap = true,
-           modifier = Modifier.padding(horizontal = 16.dp)
-       )
+        middleText?.let {
+           Text(
+               text = it,
+               textAlign = TextAlign.Center,
+               softWrap = true,
+               modifier = Modifier.padding(horizontal = 16.dp),
+               fontWeight = FontWeight.W400
+           )
 
-       Text(text = "- Paulina Simons")
+        }
+
+        bottomText?.let {
+           Text(
+               text = it,
+               fontWeight = FontWeight.W300
+           )
+        }
 
     }
 }
@@ -35,5 +58,10 @@ fun AnswerAlertPanel() {
 @Preview(showBackground = true)
 @Composable
 private fun AnswerAlertPanelPreview() {
-    AnswerAlertPanel()
+    AnswerAlertPanel(
+        topBadge = Icons.Default.Done,
+        topText = "Correct Answer",
+        middleText = "\"All good things come to those who wait.\"",
+        bottomText = "- Paulina Simons"
+    )
 }
