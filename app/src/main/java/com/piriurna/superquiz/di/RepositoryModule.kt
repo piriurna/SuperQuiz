d59bcdb0fa0ff33f1c680fd60e73f00b5ef886a7
Franco <Zalamena>
2022-08-03 12:15:39 +0100

    RepositoryModule

---
+++ app/src/main/java/com/piriurna/superquiz/di/RepositoryModule.kt
@@ -0,0 +1,25 @@
+package com.piriurna.superquiz.di
+
+import com.piriurna.data.remote.sources.TriviaApiSource
+import com.piriurna.data.repositories.TriviaRepositoryImpl
+import com.piriurna.domain.repositories.TriviaRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+
+    @Provides
+    @Singleton
+    fun provideTriviaRepository(dogApiSource: TriviaApiSource): TriviaRepository {
+        return TriviaRepositoryImpl(dogApiSource)
+    }
+
+
+
+}
