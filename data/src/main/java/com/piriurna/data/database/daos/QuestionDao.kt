dfa8ced544d7501abbd1d739808a447ce0b8072c
Franco <Zalamena>
2022-08-10 17:08:16 +0100

    Returning list of ids

--- data/src/main/java/com/piriurna/data/database/daos/QuestionDao.kt
+++ data/src/main/java/com/piriurna/data/database/daos/QuestionDao.kt
@@ -9,7 +9,7 @@ import com.piriurna.data.database.entities.QuestionEntity
 @Dao
 interface QuestionDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertQuestions(questions: List<QuestionEntity>)
+    suspend fun insertQuestions(questions: List<QuestionEntity>) : List<Long>
 
     @Query("SELECT * FROM QUESTION WHERE categoryId =:categoryId")
     suspend fun getQuestions(categoryId: Int): List<QuestionEntity>?
