e620374c1fa2968ec68c07c47642af25fed994f7
Franco <Zalamena>
2022-09-07 19:48:17 +0100

    SQViewPagerIndicator with animation on the dots

--- common/src/main/java/com/piriurna/common/composables/viewpager/SQViewPagerIndicatorDot.kt
+++ common/src/main/java/com/piriurna/common/composables/viewpager/SQViewPagerIndicatorDot.kt
@@ -1,19 +1,19 @@
 package com.piriurna.common.composables.viewpager
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.piriurna.common.theme.lightPurple
 import com.piriurna.common.theme.purple
 
 
@@ -23,24 +23,19 @@ fun SQViewPagerIndicatorDot(
     selected: Boolean,
     modifier: Modifier = Modifier
 ) {
-    Box(modifier = modifier) {
-        if(selected) {
-            Surface(
-                shape = RoundedCornerShape(15.dp),
-                color = color,
-                modifier = Modifier
-                    .width(48.dp)
-                    .height(8.dp)
-            ) {
+    val width = animateDpAsState(targetValue = if (selected) 48.dp else 12.dp)
 
-            }
-        } else {
-            Box(
-                modifier = Modifier
-                    .size(12.dp)
-                    .clip(CircleShape)
-                    .background(color)
-            )
+    val height = animateDpAsState(targetValue = if (selected) 8.dp else 12.dp)
+
+    Box(modifier = modifier) {
+        Surface(
+            shape = RoundedCornerShape(15.dp),
+            color = color,
+            modifier = Modifier
+                .width(width = width.value)
+                .height(height = height.value)
+        ) {
+
         }
     }
 }
@@ -48,5 +43,18 @@ fun SQViewPagerIndicatorDot(
 @Preview(showBackground = true)
 @Composable
 private fun SQViewPagerIndicatorDotPreview() {
-    SQViewPagerIndicatorDot(color = purple, selected = true)
+    var selected by remember {
+        mutableStateOf(true)
+    }
+
+    Column() {
+        SQViewPagerIndicatorDot(
+            modifier = Modifier.clickable {
+                selected = !selected
+            },
+            color = purple,
+            selected = selected
+        )
+        SQViewPagerIndicatorDot(color = lightPurple, selected = !selected)
+    }
 }
