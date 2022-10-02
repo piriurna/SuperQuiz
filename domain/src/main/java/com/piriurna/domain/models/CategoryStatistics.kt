7adba6fedb1090d22ca08c50601e8b739408aaa2
Franco <francozalamena@gmail.com>
2022-10-02 14:53:51 +0100

    Changed the success screen to show a fail one as well

--- domain/src/main/java/com/piriurna/domain/models/CategoryStatistics.kt
+++ domain/src/main/java/com/piriurna/domain/models/CategoryStatistics.kt
@@ -4,7 +4,7 @@ data class CategoryStatistics(
     val categoryId: Int = 0,
     val totalNumberOfQuestions: Int = 0,
     val correctAnswers : Int = 0,
-    val incorrectAnswers: Int = 0
+    val incorrectAnswers: Int = 0,
 ) {
     
     constructor(categoryId: Int, questions: List<Question>) : this(
@@ -14,5 +14,8 @@ data class CategoryStatistics(
         incorrectAnswers = questions.count { it.isQuestionAnsweredIncorrectly() }
     )
 
+    fun isSuccess() = getPercentageOfCorrectAnswers() > 55
+
+    fun getPercentageOfCorrectAnswers() = try { correctAnswers * 100 / totalNumberOfQuestions } catch (e : ArithmeticException) { 0 }
     fun getNotAnsweredQuestions() = totalNumberOfQuestions - correctAnswers - incorrectAnswers
 }
