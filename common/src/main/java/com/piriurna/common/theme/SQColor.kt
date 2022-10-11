1b38104b354812efb434798cab27ebbdb3ee1d12
Franco <francozalamena@gmail.com>
2022-10-11 21:52:01 +0100

    Movem Style related classes to Common

--- common/src/main/java/com/piriurna/common/theme/SQColor.kt
+++ common/src/main/java/com/piriurna/common/theme/SQColor.kt
@@ -4,6 +4,13 @@ import androidx.compose.material.Colors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
 @get:Composable
 val Colors.secondaryBackground: Color
     get() = if (isLight) Color(0xffffffff) else Color(0xff252525)
@@ -14,7 +21,7 @@ val Colors.primaryText: Color
 
 
 val purple = Color(74, 21, 173)
-val lightPurple = Color(red = 149, green = 67, blue = 255, alpha = 26)
+val lightPurple = Color(74, 21, 173, alpha = 26)
 
 val orange = Color(red = 254, green = 177, blue = 87)
 val lightOrange = Color(red = 254, green = 177, blue = 87, alpha = 26)
@@ -32,3 +39,7 @@ val progressBlue = Color(red = 0, green = 101, blue = 253)
 val incompleteGray = Color(red = 237, green = 234, blue = 255)
 
 val errorColor = Color(192, 57, 43)
+
+val gradientInnerColor = Color(146, 68, 248)
+val gradientCentralColor = Color(138, 64, 249)
+val gradientOuterColor = Color(77, 40, 218)
---
+++ common/src/main/java/com/piriurna/common/theme/SQShape.kt
@@ -0,0 +1,13 @@
+package com.piriurna.common.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+object SQShape {
+    val Shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(4.dp),
+        large = RoundedCornerShape(0.dp)
+    )
+}
