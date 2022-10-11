0f9fd5927963c2d47eb229745212fc7e34e9c3b9
Franco <francozalamena@gmail.com>
2022-10-11 19:56:32 +0100

    Swipe to refresh added in the play games screen

--- app/src/main/java/com/piriurna/superquiz/presentation/playgames/PlayGamesViewModel.kt
+++ app/src/main/java/com/piriurna/superquiz/presentation/playgames/PlayGamesViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.viewModelScope
 import com.piriurna.domain.Resource
 import com.piriurna.domain.usecases.GetCategoriesUseCase
 import com.piriurna.domain.usecases.GetProfileSettingsUseCase
+import com.piriurna.domain.usecases.RefreshCategoriesUseCase
 import com.piriurna.superquiz.SQBaseEventViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
@@ -13,7 +14,8 @@ import javax.inject.Inject
 @HiltViewModel
 class PlayGamesViewModel @Inject constructor(
     private val getCategoriesUseCase: GetCategoriesUseCase,
-    private val getProfileSettingsUseCase: GetProfileSettingsUseCase
+    private val getProfileSettingsUseCase: GetProfileSettingsUseCase,
+    private val refreshCategoriesUseCase: RefreshCategoriesUseCase
 ) : SQBaseEventViewModel<PlayGamesEvents>(){
 
 
@@ -29,6 +31,10 @@ class PlayGamesViewModel @Inject constructor(
         when(event) {
             is PlayGamesEvents.GetData -> {
                 getUserData()
+            }
+
+            is PlayGamesEvents.RefreshCategories -> {
+                refreshCategories()
             }
         }
     }
@@ -50,6 +56,24 @@ class PlayGamesViewModel @Inject constructor(
                         isLoading = true
                     )
 
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun refreshCategories() {
+        refreshCategoriesUseCase().onEach { result ->
+            when(result) {
+                is Resource.Loading -> {
+                    _state.value = _state.value.copy(
+                        isRefreshing = true
+                    )
+                }
+
+                else -> {
+                    _state.value = _state.value.copy(
+                        isRefreshing = false
+                    )
                 }
             }
         }.launchIn(viewModelScope)
