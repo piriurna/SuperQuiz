13b7a3ce700c5d7d63fe6a2711c177873e0775b7
Franco <francozalamena@gmail.com>
2022-10-15 18:18:31 +0100

    Fixed crash with int typefor category id

--- app/src/main/java/com/piriurna/superquiz/presentation/navigation/PlayGamesNavigationGraph.kt
+++ app/src/main/java/com/piriurna/superquiz/presentation/navigation/PlayGamesNavigationGraph.kt
@@ -2,6 +2,7 @@ package com.piriurna.superquiz.presentation.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import com.piriurna.common.composables.navigation.BaseDestinations
@@ -40,6 +41,6 @@ sealed class PlayGamesDestinations(
     val route: String,
     customArguments: List<CustomArguments> = emptyList()
 ) : BaseDestinations(route = route, customArguments = customArguments) {
-    object Questions : PlayGamesDestinations(route = "QUESTIONS", listOf(CustomArguments(NavigationArguments.CATEGORY_ID)))
+    object Questions : PlayGamesDestinations(route = "QUESTIONS", listOf(CustomArguments(NavigationArguments.CATEGORY_ID, NavType.IntType, nullable = false)))
     object CategoryCompleted : PlayGamesDestinations(route = "CATEGORY_COMPLETED", listOf(CustomArguments(NavigationArguments.CATEGORY_ID)))
 }
